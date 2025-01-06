d88a7a079885e2303939b4503edc8ee7926a1423
Dan Nemirovski <dan7.nm@gmail.com>
2025-01-06 16:34:41 +0200

    Deleted a print

--- src/multi_qubit.py
+++ src/multi_qubit.py
@@ -642,7 +642,6 @@ class MultiQubit:
         
         for _ in range(num_of_measurements):
             collapsed_state = self.measure()
-            print(collapsed_state)
             random_variable_list.append(int(collapsed_state,2))
 
         return random_variable_list
